23ad2d4629b7d991fba4125ee245ac285c551ee9
Erick Bravo <erickbravo@MBP.local>
2021-12-08 11:54:01 -0800

    mikrorm setup

--- lireddit-server/dist/index.js
+++ lireddit-server/dist/index.js
@@ -1,2 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [],
+        dbName: 'lireddit',
+        user: "erickbravo",
+        password: "scaryterry",
+        debug: !constants_1.__prod__,
+        type: "postgresql",
+    });
+};
+main();
 console.log("Goodbye world");
 //# sourceMappingURL=index.js.map
--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,1 +1,17 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [],
+        dbName: 'lireddit',
+        user: "erickbravo",
+        password: "scaryterry",
+        debug: !__prod__,
+        type: "postgresql",
+    });
+};
+
+main();
+
 console.log("Goodbye world");
