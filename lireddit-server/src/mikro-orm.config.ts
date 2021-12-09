b9a0266c9e90b03edd7a01c6bd292154df6f1ab7
Erick Bravo <erickbravo@MBP.local>
2021-12-09 14:16:50 -0800

    mikro-orm config

---
+++ lireddit-server/src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { Options } from "@mikro-orm/core";
+import path from "path";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const config: Options = {
+  migrations: {
+    path: path.join(__dirname, './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  user: "erickbravo",
+  password: "scaryterry",
+  debug: !__prod__,
+  type: "postgresql",
+};
+
+export default config
+
