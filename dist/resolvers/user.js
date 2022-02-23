6e04efe6de0f71a346235adf16f58d2972cde19b
Erick-Bravo <erickbrvo@gmail.com>
2022-02-23 11:54:27 -0800

    login mutation

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -32,15 +32,70 @@ __decorate([
 UsernamePasswordInput = __decorate([
     (0, type_graphql_1.InputType)()
 ], UsernamePasswordInput);
+let FieldError = class FieldError {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    (0, type_graphql_1.Field)(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], UserResponse);
 let UserResolver = class UserResolver {
     async register(options, { em }) {
         const hashedpassword = await argon2_1.default.hash(options.password);
         const user = em.create(User_1.User, {
-            username: options.username,
+            username: options.username.toLocaleLowerCase(),
             password: hashedpassword,
         });
         await em.persistAndFlush(user);
-        return User_1.User;
+        return user;
+    }
+    async login(options, { em }) {
+        const user = await em.findOne(User_1.User, {
+            username: options.username.toLocaleLowerCase(),
+        });
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username or password",
+                        message: "username or password are incorrect",
+                    },
+                ],
+            };
+        }
+        const validate = await argon2_1.default.verify(user.password, options.password);
+        if (!validate) {
+            return {
+                errors: [
+                    {
+                        field: "username or password",
+                        message: "username or password are incorrect",
+                    },
+                ],
+            };
+        }
+        return {
+            user
+        };
     }
 };
 __decorate([
@@ -51,6 +106,14 @@ __decorate([
     __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => UserResponse),
+    __param(0, (0, type_graphql_1.Arg)("options")),
+    __param(1, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     (0, type_graphql_1.Resolver)()
 ], UserResolver);
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,15 +1,41 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver,  } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 import argon2 from "argon2";
 
 //Different way you can use arguments in GraphQL
+//Input types we use for Mutations
 @InputType()
 class UsernamePasswordInput {
   @Field()
   username: string;
   @Field()
   password: string;
+}
+
+//Object types we use for Arguments
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
@@ -21,10 +47,44 @@ export class UserResolver {
   ) {
     const hashedpassword = await argon2.hash(options.password);
     const user = em.create(User, {
-      username: options.username,
+      username: options.username.toLocaleLowerCase(),
       password: hashedpassword,
     });
     await em.persistAndFlush(user);
-    return User;
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username.toLocaleLowerCase(),
+    });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username or password",
+            message: "username or password are incorrect",
+          },
+        ],
+      };
+    }
+    const validate = await argon2.verify(user.password, options.password);
+    if (!validate) {
+      return {
+        errors: [
+          {
+            field: "username or password",
+            message: "username or password are incorrect",
+          },
+        ],
+      };
+    }
+    return {
+        user
+    };
   }
 }
