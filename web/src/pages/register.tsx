57cd3bdaf5425c89ee71ebe33d8311149f38eb30
Erick-Bravo <erickbrvo@gmail.com>
2022-03-01 16:39:42 -0800

    Wrapper and InputField. Register created

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React from "react";
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import Wrapper from "../components/Wrapper";
+import InputField from "../components/InputField";
 
-interface registerProps {
-
-}
+interface registerProps {}
 
 const register: React.FC<registerProps> = ({}) => {
-        return (
-            <div>register page</div>
-        );
-}
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              colorScheme="teal"
+              isLoading={isSubmitting}
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
 
-export default register
+export default register;
